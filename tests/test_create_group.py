3ec10dd63be60b36f4460c68ae69691cb65fbd8d
Svitlana Oliinyk <Svitlana Oliinyk>
2017-04-22 19:01:39 +0300

    add tests with verification message

--- tests/test_create_group.py
+++ tests/test_create_group.py
@@ -5,7 +5,8 @@ def test_group(app):
     test_data_group = Group(name='Client', header='SuperHead', footer='Супер')
     app.open_group_page()
     app.create_group(test_data_group)
-    # TODO: Verify message for group creation
+    # Verification
+    assert 'A new group has been entered into the address book.' in app.message()
     app.return_to_group_page()
     # TODO: Verify group in grouplist
 
--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -4,6 +4,7 @@ def test_delete_group(app):
 
     app.open_group_page()
     app.delete_group_by_number(0)
-    # TODO: Verify message
+    # Verification
+    assert 'Group has been removed.' in app.message()
     app.return_to_group_page()
     # TODO: Verify deletion group in list
