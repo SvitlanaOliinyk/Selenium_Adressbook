6211ed8a04a67910efa416819d87b1c84d6cd410
Svitlana Oliinyk <Svitlana Oliinyk>
2017-04-22 18:49:37 +0300

    fixures for all tests

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from web_api.adressbook_api import AdressbookAPI
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def app():
     app = AdressbookAPI()
     app.login(username="admin", password="secret")
