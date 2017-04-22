fa5ed3f01bc90bdfb47707e9a474ae1e5bfa125a
Svitlana Oliinyk <Svitlana Oliinyk>
2017-04-22 16:48:44 +0300

    Add parametrization login and password

--- create_group.py
+++ create_group.py
@@ -9,33 +9,36 @@ class CreateGroup(unittest.TestCase):
         self.wd.implicitly_wait(10)
     
     def test_(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.open_group_page(wd)
-        self.create_group(wd)
-        self.return_to_group_page(wd)
-        self.logout(wd)
+        self.open_home_page()
+        self.login("admin", "secret")
+        self.open_group_page()
+        self.create_group()
+        self.return_to_group_page()
+        self.logout()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
         # Open homepage
+        wd = self.wd
         wd.get("http://localhost:8888/addressbook/")
 
-    def login(self, wd):
+    def login(self, login, password):
         # login
+        wd = self.wd
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(login)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
 
-    def open_group_page(self, wd):
+    def open_group_page(self):
         # Open group page
+        wd = self.wd
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def create_group(self, wd):
+    def create_group(self):
         # Init group creation
+        wd = self.wd
         wd.find_element_by_xpath(r'//*[@id="nav"]/ul/li[3]/a').click()
         # Fill group form
         wd.find_element_by_name("new").click()
@@ -54,12 +57,14 @@ class CreateGroup(unittest.TestCase):
         # Submit group form
         wd.find_element_by_name("submit").click()
 
-    def return_to_group_page(self, wd):
+    def return_to_group_page(self):
         # Return to group page
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def logout(self, wd):
+    def logout(self):
         # Logout
+        wd = self.wd
         wd.find_element_by_css_selector('form[name="logout"] > a').click()
 
     def tearDown(self):
